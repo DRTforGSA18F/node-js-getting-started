e8605c6605e588aa9b3cc5784f3151fb0b6d23f4
Yonas Robi <Yonas.robi@cgifederal.com>
2015-05-14 10:40:57 -0400

    adding counter to index route

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var test = require('./models/mongolab.js');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
 });
 
 app.listen(app.get('port'), function() {
